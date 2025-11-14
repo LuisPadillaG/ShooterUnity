24ecfad868997ef1018b6f83e4a9c5f5597192c9
Luis Alberto Padilla González <luis.gle3@gmail.com>
2025-11-14 14:49:28 -0700

    ACTUALIZA Zombie y SKYBOX
    Puse un skybox de prueba, un enlace en el script de zombie a un tutorial para la niebla y la inclusión de NavMeshSurface para el zombie. Limpiar y borrar archivos del juego

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
     DatosJugador datosJugador;
     GameObject jugador;
-    Vector3 rotacion, posicion;
+    Vector3 rotacion;
+    //Vector3 posicion;
     GameObject modelo, DatosJuego;
     Animator zombieAnimator;
     public float puntosMiZombie;
     float velocidadZombie;
     AudioSource zombieFar_uno;
+    NavMeshAgent navMeshAgent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,10 +24,11 @@ public class Zombie : MonoBehaviour
         zombieAnimator = modelo.GetComponent<Animator>();
         jugador = GameObject.FindGameObjectWithTag("Player");
         rotacion = Vector3.zero;
-        posicion = this.transform.position;
+        //posicion = this.transform.position;
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
         puntosMiZombie = 50;
-        zombieFar_uno = this.transform.GetChild(1).GetChild(0).GetChild(0).GetComponent<AudioSource>() ;
-        // calculo de datos MiZomnbie
+        zombieFar_uno = this.transform.GetChild(1).GetChild(0).GetChild(0).GetComponent<AudioSource>();
+        // calculo de datos MiZombie 
         for (int i = 0; i < datosJugador.RondaActual; i++) {
             Debug.Log("Zombie de la ronda "+ i);
             puntosMiZombie += 100;
@@ -49,9 +53,9 @@ public class Zombie : MonoBehaviour
         this.transform.rotation = Quaternion.Euler(rotacion);
         rotacion.y = HerramientasGenericas.CalcularAnguloBidimensional(new Vector2(this.transform.position.x, this.transform.position.z), new Vector2(jugador.transform.position.x, jugador.transform.position.z));
 
-        posicion.x += Mathf.Cos(rotacion.y * Mathf.Deg2Rad) * Time.deltaTime * velocidadZombie;
+        /*posicion.x += Mathf.Cos(rotacion.y * Mathf.Deg2Rad) * Time.deltaTime * velocidadZombie;
         posicion.z -= Mathf.Sin(rotacion.y * Mathf.Deg2Rad) * Time.deltaTime * velocidadZombie;
-        this.transform.position = posicion;
+        this.transform.position = posicion;*/
         // Animaciones
         if(velocidadZombie == 1.3f || velocidadZombie == 1.6f) //caminando, caminando rapido
         {
@@ -66,6 +70,7 @@ public class Zombie : MonoBehaviour
         1. Golpe de brazo + animación 
         2. Recibir balazo
          */
+        navMeshAgent.SetDestination(jugador.transform.position);
     }
     public void RecibirDisparo(float danoDisparo)
     {
@@ -73,5 +78,5 @@ public class Zombie : MonoBehaviour
         zombieAnimator.SetInteger("estado", 2);  
         Debug.Log("Zombie recibió un disparo. Vida restante: " + puntosMiZombie);
     }
-
+    // tutorial para las particulas de la niebla: https://youtu.be/8pgi1TBGCKM?si=lwtmWtJ4o1i1UxE6
 }
